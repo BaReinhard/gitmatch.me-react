116366b12b48b0dec924119745292d55a2bdb2e3
Brett Reinhard <Brett@Bretts-MacBook-Pro-2.local>
2017-07-26 13:12:15 -0700

    added uniquelanguages to rresultcard, needs styling and more thought towards what should actually be passed down

--- src/components/common/resultcard.js
+++ src/components/common/resultcard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 export default function GitMatchResultCard(props) {
-	if (props.user.score === undefined) {
+	if (props.user.score === undefined && props.user.topLanguages) {
+		console.log(props.user);
 		return (
 			<div>
 				<h3>Match</h3>
 				<h4>
 					{props.user.userData.login}
 				</h4>
-
+				{props.user.topLanguages.map(lang => lang.language)}
 				<hr />
 			</div>
 		);
-	} else {
+	} else if (props.key !== null) {
 		return (
 			<div key={props.key}>
 				<h3>Top Match</h3>
@@ -27,5 +28,7 @@ export default function GitMatchResultCard(props) {
 				<hr />
 			</div>
 		);
+	} else {
+		return <div />;
 	}
 }
